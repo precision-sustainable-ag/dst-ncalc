24248ea3701f95efebf8b5e1b82e6165bd17d0f5
Rick Hitchcock <rickhitchcock@outlook.com>
2022-10-07 11:50:47 -0400

    React 18

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
+import {createRoot} from 'react-dom/client';
 import {Provider} from 'react-redux';
 import {store} from './store/Store';
-
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-if (false) {
-  ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// breaks Map:
+/*
+  root.render(
     <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
   );
-} else {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root')
-  );
-}
+*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
