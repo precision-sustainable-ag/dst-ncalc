573b59f2b3312f548357d50348588fb4a22fc002
milad <milad@nyu.edu>
2023-01-11 23:18:42 -0500

    fixed persistant shapes and zoom

--- src/shared/Map/index.js
+++ src/shared/Map/index.js
@@ -1,35 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import { Map } from '@psa/dst.ui.map';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import mapboxgl from 'mapbox-gl';
-import { set } from '../../store/Store';
+import { get, set } from '../../store/Store';
 import './styles.scss';
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
 mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
+let removedShapes = new Set();
+
 const MapComp = () => {
   const [address, setAddress] = useState({});
-  const [geometry, setGeometry] = useState([]);
+  const [zoom, setZoom] = useState(null);
   const dispatch = useDispatch();
-  const initStartZoom = 10;
+  const lat = useSelector(get.lat);
+  const lon = useSelector(get.lon);
+  const mapZoom = useSelector(get.mapZoom);
+  const mapPolygon = useSelector(get.mapPolygon);
+  const [features, setFeatures] = useState(mapPolygon);
+  const [drawEvent, setDrawEvent] = useState({});
+  // const [removedShapes, setRemovedShapes] = useState(new Set());
+
   useEffect(() => {
-    dispatch(set.mapPolygon(geometry));
-    dispatch(set.mapZoom(initStartZoom));
+    if (drawEvent.mode === 'delete') {
+      removedShapes = removedShapes.add(drawEvent.e.features[0].id);
+    }
+    const ids = new Set(mapPolygon.map((d) => d.id));
+    const merged = [
+      ...mapPolygon.filter((d) => !removedShapes.has(d.id)),
+      ...features.filter((d) => !ids.has(d.id) && !removedShapes.has(d.id)),
+    ];
+    dispatch(set.mapPolygon(merged));
+  }, [drawEvent]);
+
+  useEffect(() => {
     dispatch(set.mapType('satellite'));
     if (address.latitude) dispatch(set.lat(address.latitude));
     if (address.longitude) dispatch(set.lon(address.longitude));
-  }, [address, geometry, dispatch]);
+  }, [address, dispatch]);
+
+  useEffect(() => {
+    if (zoom) dispatch(set.mapZoom(zoom));
+  }, [zoom]);
 
   return (
     <div className="map">
       <Map
         setAddress={setAddress}
-        setGeometry={setGeometry}
+        setFeatures={setFeatures}
+        setZoom={setZoom}
+        onDraw={setDrawEvent}
+        initFeatures={mapPolygon}
         initWidth="100%"
         initHeight="400px"
-        initLon={-80.16}
-        initLat={37.75}
-        initStartZoom={initStartZoom}
+        initLon={lon}
+        initLat={lat}
+        initStartZoom={mapZoom}
         initMinZoom={5}
         initMaxZoom={16}
         hasSearchBar
