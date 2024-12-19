e1faf4ffd15297819021eb016a7d5abc9884a5ec
Ravi Ghevariya <ravighevariya10@gmail.com>
2024-12-19 14:00:26 -0500

    Integrated the shared Accordion

--- src/components/Location/index.jsx
+++ src/components/Location/index.jsx
@@ -1,47 +1,24 @@
-/* eslint-disable operator-linebreak */
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-// import * as turf from '@turf/turf';
-// import axios from 'axios';
-import Box from '@mui/material/Box';
-import { Grid } from '@mui/material';
-import Paper from '@mui/material/Paper';
-import Badge from '@mui/material/Badge';
-import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
-import Accordion from '@mui/material/Accordion';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
 import { useDispatch, useSelector } from 'react-redux';
-import { PSALoadingSpinner, PSATooltip, PSATextField } from 'shared-react-components/src';
+import { Box, Stack, Typography, Badge, Paper, Grid } from '@mui/material';
+import { PSALoadingSpinner, PSATooltip, PSAAccordion } from 'shared-react-components/src';
 import BiomassMap from '../../shared/Map/BiomassMap';
-// import NitrogenMap from '../Map/NitrogenMap';
+import Input from '../../shared/Inputs';
 import Help from '../../shared/Help';
 import { get, set } from '../../store/Store';
 import NavButton from '../../shared/Navigate/NavButton';
 import useFetchHLS from '../../hooks/useFetchHLS';
 import Datebox from '../../shared/BiomassData/Datebox';
 import { AreaErrorModal, TaskFailModal } from '../../shared/BiomassData/Warnings';
-import { historyStates } from '../../store/inits';
-
-const CustomizedAccordion = styled(Accordion)(() => ({
-  '&.MuiPaper-root': {
-    borderRadius: '1rem',
-    boxShadow: 'none',
-  },
-  boxShadow: 'none',
-}));
 
 const nextButtonBadgeContent = () => (
-  <PSATooltip
-    title="No polygon is drawn"
-    tooltipContent={(
-      <Typography>?</Typography>
+  <PSATooltip title="No polygon is drawn" tooltipContent={(
+    <Typography>?</Typography>
   )}
   />
 );
+
 // TODO: barebone is a var to decide if this view is showed as a widget, same in other pages
 const Location = ({ barebone = false }) => {
   const navigate = useNavigate();
@@ -51,69 +28,49 @@ const Location = ({ barebone = false }) => {
   const biomassTaskResults = useSelector(get.biomassTaskResults);
   const polyDrawTooBig = useSelector(get.polyDrawTooBig);
   const biomassFetchIsFailed = useSelector(get.biomassFetchIsFailed);
-  const field = useSelector(get.field);
-  const { historyState, userHistoryList } = useSelector(get.user);
 
-  const [fieldName, setFieldName] = useState(field);
-
-  // api for getting biomass map
+  // API for getting biomass map
   useFetchHLS();
-
-  const isFieldNameExisted = () => {
-    if (historyState === historyStates.imported) return false;
-    const result = userHistoryList.find((history) => history.label === 'history-'.concat(fieldName));
-    return result !== undefined;
-  };
 
   return (
     <Box sx={{ width: '100%', padding: '0rem' }}>
       <Box mb={-2}>
-        <CustomizedAccordion defaultExpanded>
-          <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
-            {!barebone && (
-              <Typography variant="h5" gutterBottom>
-                Where is your Field located?
-              </Typography>
-            )}
-          </AccordionSummary>
-          <AccordionDetails>
-            <Stack mb={1}>
-              <Typography variant="h8" gutterBottom>
-                Enter your address or zip code to determine your field&apos;s location. You can then zoom in and click to pinpoint it on the map. If
-                you know your exact coordinates, you can enter them in search bar separated by comma (ex. 37.7, -80.2 ).
-              </Typography>
-              {isSatelliteMode && (
-                <Typography variant="h8" gutterBottom pt={1}>
-                  Specify your field&apos;s boundary on the map using the drawing tool.
-                </Typography>
-              )}
-            </Stack>
-            <Box mb={2}>
-              <PSATextField
-                label="Name your Field (optional)"
-                value={fieldName}
-                onChange={(e) => setFieldName(e.target.value)}
-                error={isFieldNameExisted()}
-                helperText={isFieldNameExisted() ? 'Field name existed!' : null}
-                onBlur={() => {
-                  if (!isFieldNameExisted()) dispatch(set.field(fieldName));
-                }}
-                onKeyDown={(e) => {
-                  if (e.key === 'Enter' && !isFieldNameExisted()) dispatch(set.field(fieldName));
-                }}
-              />
-              <Help />
-            </Box>
-            <Stack mt={5} gap={1}>
-              {isSatelliteMode && (
+        <PSAAccordion
+          expanded
+          summaryContent={!barebone && (
+            <Typography variant="h5" gutterBottom>
+              Where is your Field located?
+            </Typography>
+          )}
+          detailsContent={(
+            <>
+              <Stack mb={1}>
                 <Typography variant="h8" gutterBottom>
-                  Specify your crop&apos;s planting and termination dates.
+                  Enter your address or zip code to determine your field&apos;s location. You can then zoom in and click to pinpoint it on the map. If
+                  you know your exact coordinates, you can enter them in search bar separated by comma (ex. 37.7, -80.2 ).
                 </Typography>
-              )}
-              <Datebox />
-            </Stack>
-          </AccordionDetails>
-        </CustomizedAccordion>
+                {isSatelliteMode && (
+                  <Typography variant="h8" gutterBottom pt={1}>
+                    Specify your field&apos;s boundary on the map using the drawing tool.
+                  </Typography>
+                )}
+              </Stack>
+              <Box mb={2}>
+                <Input label="Name your Field (optional)" id="field" autoComplete="off" style={{ height: '2rem', minWidth: '13rem' }} />
+                <Help />
+              </Box>
+              <Stack mt={5} gap={1}>
+                {isSatelliteMode && (
+                  <Typography variant="h8" gutterBottom>
+                    Specify your crop&apos;s planting and termination dates.
+                  </Typography>
+                )}
+                <Datebox />
+              </Stack>
+            </>
+          )}
+          testId="location-accordion"
+        />
       </Box>
       <Box sx={{ margin: '2rem 0rem' }}>
         <Paper sx={{ padding: '1rem', borderRadius: '1rem' }}>
@@ -146,8 +103,12 @@ const Location = ({ barebone = false }) => {
               </Typography>
             </Box>
           )}
-          {polyDrawTooBig && <AreaErrorModal />}
-          {biomassFetchIsFailed && <TaskFailModal task="biomass" />}
+          {polyDrawTooBig && (
+            <AreaErrorModal />
+          )}
+          {biomassFetchIsFailed && (
+            <TaskFailModal task="biomass" />
+          )}
           <BiomassMap variant="biomass" />
           {!barebone && (
             <Box
@@ -192,6 +153,6 @@ const Location = ({ barebone = false }) => {
       </Box>
     </Box>
   );
-}; // Location
+};
 
 export default Location;
