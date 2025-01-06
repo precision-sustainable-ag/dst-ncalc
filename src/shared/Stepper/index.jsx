c28878f36e9bafbb92fcfc547c13555c9e528f07
Jingtong Xu <xjt980408@gmail.com>
2025-01-06 14:32:27 -0500

    update mobile stepper

--- src/shared/Stepper/index.jsx
+++ src/shared/Stepper/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { Box, useMediaQuery, useTheme } from '@mui/material';
 import { PSAStepper } from 'shared-react-components/src';
 import { get, set } from '../../store/Store';
 
@@ -8,26 +9,31 @@ const tabs = ['home', 'location', 'soil', 'covercrop', 'cashcrop', 'output'];
 const titles = ['Home', 'Location', 'Soil', 'Cover Crop', 'Cash Crop', 'Output'];
 
 const NcalcStepper = () => {
+  const theme = useTheme();
+  const matchesMd = useMediaQuery(theme.breakpoints.down('md'));
 
   const step = useSelector(get.activeStep);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  
-  const handleClickStep = (stepName, index) => {
+
+  const handleClickStep = (index) => {
     dispatch(set.activeStep(index));
     navigate(`/${tabs[index]}`);
   };
 
   return (
-
-    <PSAStepper 
-      steps = { titles }
-      onStepClick = { handleClickStep }
-      strokeColor = "white"
-      stepperProps={{ activeStep: step }}
-    />
-
+    <Box sx={{ p: matchesMd ? 0 : '1rem' }}>
+      <PSAStepper
+        steps={titles}
+        onStepClick={handleClickStep}
+        strokeColor="white"
+        stepperProps={{ activeStep: step }}
+        mobile={matchesMd}
+        // TODO: add disable next logic
+        nextButtonDisabled={false}
+      />
+    </Box>
   );
 };
 
